cdc321b4858e30bacc384c18d2bfd6f0f2ea83bc
Pavel <ubuntu@ubuntu.ru>
2023-07-03 18:03:31 +0500

    Start full item detail

--- src/api/main.js
+++ src/api/main.js
@@ -4,4 +4,10 @@ async function getApiProducts(){
     let data = await fetch(`${getBaseUrl()}/products?offset=0&limit=100`).then(res=>res.json())
     return data
 }
+
+async function getApiProduct(id){
+    let data = await fetch($`${getBaseUrl()}/products/${id}`)
+    return data
+}
+async function get
 export default getApiProducts;
---
+++ src/components/FullItem.vue
@@ -0,0 +1,7 @@
+<template>
+    Full item preview
+</template>
+<script>
+</script>
+<style scoped>
+</style>
